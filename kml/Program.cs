b5aa8168d3c3373d36f5ce338af1470aa1d070ba
Jacob Chaar <jacobchaar@live.ca>
2018-05-18 16:16:47 -0400

    KML Developpment from SQLite database by and process the data with C#

---
+++ kml/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Import all these librairys note that the Newtonsoft, SQLite and Sharpkml are opens sources librayries from the Nuget package.
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml.Linq;
+using System.Data.SQLite;
+using SharpKml.Base;
+using SharpKml.Dom;
+using SharpKml.Engine;
+
+namespace kml
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+      
+            //create list of object named logfile. the logfile is a public struct in the file database_connection
+            List<logfile> dataresults = new List<logfile>();
+
+
+            database_connection conecct_toDB = new database_connection();
+
+            //connect the database.
+            conecct_toDB.connect();
+
+            //list constaint all the element of the DB with their properties and values
+            List<logfile> test = new List<logfile>();
+           
+            //the ExecuteSQL will return all element need for displaying into the Google earth
+            test = conecct_toDB.ExecuteSQL("SELECT location.id, location.json, location.lat, location.lng, location.synced, photo.filename from location join photo on location.id = photo.location_id");
+
+        
+            //begin of the creation of kml file 
+           
+            Kml kml = new Kml();
+            Document document = new Document();
+            kml.Feature = document; 
+
+            foreach (logfile x in test)
+            {
+              
+                Point point = new Point();
+                Placemark placemark = new Placemark();
+                ExtendedData extenddata = new ExtendedData();
+                Data databearing = new Data();
+                Data datadistance = new Data();
+                Data datalat = new Data();
+                Data datalon = new Data();
+                Data datasource = new Data();
+                Data datasynced = new Data();
+                Data dataID = new Data();
+                Data dataLandUseType = new Data();
+
+                    
+                databearing.Name = "bearing";
+                databearing.Value = x.bearing.ToString();
+
+                datadistance.Name = "distance";
+                datadistance.Value = x.distance.ToString();
+
+                datalat.Name = "lat";
+                datalat.Value = x.lat.ToString();
+
+                datalon.Name = "lon";
+                datalon.Value = x.lng.ToString();
+
+                datasource.Name = "source";
+                datasource.Value = x.source;
+
+                datasynced.Name = "synced";
+                datasynced.Value = x.synced.ToString();
+
+                dataID.Name = "id";
+                dataID.Value = x.ID.ToString();
+
+                dataLandUseType.Name = "land_use_type";
+                dataLandUseType.Value = x.land_use_type.ToString();
+
+                extenddata.AddData(databearing);
+                extenddata.AddData(datadistance);
+                extenddata.AddData(datalat);
+                extenddata.AddData(datalon);
+                extenddata.AddData(datasource);
+                extenddata.AddData(datasynced);
+                extenddata.AddData(dataID);
+                extenddata.AddData(dataLandUseType);
+
+                int index = 1;
+                foreach (string singlephoto in x.photopath)
+                {
+                    
+                    Data des = new Data();
+                    string filename = Path.GetFileName(singlephoto);
+                    filename = filename.Remove(0, 4);
+                    string pathv2 = Path.GetFullPath(filename);
+                    string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"Example\Example\NewCamera\" + filename);
+                    path = path.Replace("\\", "/");
+
+                    des.Name = "photo " + index.ToString();
+                  des.Value  = string.Format("<img src=\"file:///{0}\" style=\"width:60%;height:60%; \"/>", path);
+                    extenddata.AddData(des);
+                    index++;
+                    
+                }
+
+
+
+                point.Coordinate = new Vector(x.lat, x.lng);
+
+
+            
+
+                placemark.Geometry = point;
+                placemark.ExtendedData = extenddata;
+                
+                document.AddFeature(placemark);
+                
+
+            }
+
+
+            //write the kml file
+            KmlFile kmlfile = KmlFile.Create(kml, false);
+            using (var stream = File.OpenWrite(Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"Example\Example\" + "kmlfile.kml")))
+            {
+                kmlfile.Save(stream);
+            }
+
+            //write the kmz file
+
+
+            Console.WriteLine("Finish");
+
+            KmzFile kmzfile = KmzFile.Create(kmlfile);
+            using (var stream = File.OpenWrite(Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"Example\Example\" + "kmzfile.kmz")))
+            {
+                kmzfile.Save(stream);
+            }
+
+
+            Console.ReadLine();
+
+        }
+       
+
+    }
+}
---
+++ kml/database_connection.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Data.SQLite;  
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+namespace kml
+{
+    //logfile struct will represent each data while looping in the DB tables
+    public struct logfile
+    {
+        public int ID { get; set; }
+        public double bearing { get; set; }
+        public int distance { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public int synced { get; set; }
+        public string source { get; set; }
+        public int land_use_type { get; set; }
+        public List<string> photopath { get; set; }
+        public logfile(int pID, double pBearing, int pDistance, double pLat, double pLng, int Psynced, string  pSource, int pLandUse, List<string> pPath )
+        {
+            ID = pID;
+            bearing = pBearing;
+            distance = pDistance;
+            lat = pLat;
+            lng = pLng;
+            synced = Psynced;
+            source = pSource;
+            land_use_type = pLandUse;
+            photopath = pPath;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\n{8}", this.ID, this.bearing, this.distance, this.lat, this.lng, this.synced, this.source, this.land_use_type, this.source, this.filename());
+           
+        }
+
+       private string filename()
+        {
+            string output="";
+
+            foreach(string x in this.photopath)
+            {
+                output += x + "\n";
+            }
+            return output;
+           
+        }
+
+
+    }
+
+    public class database_connection
+    {
+
+        public SQLiteConnection my_DBCONNECTION;
+   
+        //method for connecting the DB 
+        public void connect()
+        {
+            string connection = "Data Source="+ Path.Combine(AppDomain.CurrentDomain.BaseDirectory+ @"Example\Example\GlobalCroplands\DB.sqlite; Version =3");
+            
+           
+                my_DBCONNECTION = new SQLiteConnection(connection);
+            my_DBCONNECTION.Open();
+        }
+
+        //method for disconect the DB 
+        public void distconnect()
+        {
+            if (my_DBCONNECTION.State == ConnectionState.Open)
+                my_DBCONNECTION.Close();
+        }
+
+
+        //method for returning the data need for the kml or kmz file.
+        public List<logfile> ExecuteSQL(string sqlcode)
+        {
+            List<logfile> results = new List<logfile>();
+           
+            logfile datareadline = new logfile();
+            SQLiteCommand command = new SQLiteCommand(sqlcode, my_DBCONNECTION);
+            SQLiteCommand photocommand;
+             SQLiteDataReader reader = command.ExecuteReader();
+            SQLiteDataReader photoreader;
+            while (reader.Read())
+            {
+               
+               if (datareadline.ID != int.Parse(reader["id"].ToString()))
+                {
+                    //get every data in the result of the SQL code
+                    datareadline.ID = int.Parse(reader["id"].ToString());
+                    JObject jsondata = JObject.Parse((string)reader["json"]);
+
+                    datareadline.bearing = (double)jsondata["bearing"];
+                    datareadline.distance = (int)jsondata["distance"];
+                    datareadline.source = (string)jsondata["source"];
+                    datareadline.land_use_type = (int)jsondata["records"].Values("land_use_type").Single();
+                    datareadline.lat = (double)reader["lat"];
+                    datareadline.lng = (double)reader["lng"];
+                    datareadline.synced = int.Parse(reader["synced"].ToString());
+                   
+                    
+                    //for the photos
+                    List<string> photopath = new List<string>();
+
+                    photocommand = new SQLiteCommand(String.Format("SELECT photo.filename from photo where location_id = {0}", datareadline.ID.ToString()), my_DBCONNECTION);
+                    photoreader = photocommand.ExecuteReader();
+                    while (photoreader.Read())
+                    {
+                        photopath.Add((string)photoreader["filename"]);
+                    }
+                    datareadline.photopath = photopath;
+                 
+
+                    results.Add(datareadline);
+
+                 
+                }
+              
+              
+            }
+            return results;
+        }
+
+    }
+}
